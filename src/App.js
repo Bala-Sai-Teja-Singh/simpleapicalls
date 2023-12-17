9cadab27d84bb8bb9339ef50b0669c0abbe3054b
Bala Sai Teja Singh <balasaitejasingh@MacBook-Air.local>
2023-12-17 20:33:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [data, setData] = useState(null);
+  const [columns, setColumns] = useState(null);
+  const id = 1;
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setColumns(Object.keys(data[0]));
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Table</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            {columns?.map((column) => (
+              <th key={column}>{column}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((row) => (
+            <tr key={row.id}>
+              <td>{row.postId}</td>
+              <td>{row.id}</td>
+              <td>{row.name}</td>
+              <td>{row.email}</td>
+              <td>{row.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
